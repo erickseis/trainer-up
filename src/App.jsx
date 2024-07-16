56e7500c2c51da3f73d7972fdc6aba25e9aa889b
erick seis <erickseis@gmail.com>
2024-07-16 08:17:24 -0400

    carga

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './components/inicio';
 import './App.css';
+import Trainers from './components/trainers';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path='/' element={<Inicio />} />
+        <Route path='/trainers' element={<Trainers />} />
       </Routes>
     </HashRouter>
   );
---
+++ src/components/trainers.jsx
@@ -0,0 +1,40 @@
+const Trainers = () => {
+  return (
+    <div>
+      <img src='' alt='logo' />
+      <frame />
+      <h2>
+        Trainer: <span>CAMILO</span>
+      </h2>
+      <ul>
+        <li>Asesoramiento</li>
+        <li>Nutricion</li>
+        <li>Rutinas</li>
+        <li>Atencion 24h</li>
+        <li>Cordial</li>
+      </ul>
+      <div>
+        <ul>
+          <li>
+            <img src='' alt='' />
+          </li>
+          <li>
+            <img src='' alt='' />
+          </li>
+          <li>
+            <img src='' alt='' />
+          </li>
+          <li>
+            <img src='' alt='' />
+          </li>
+          <li>
+            <img src='' alt='' />
+          </li>
+        </ul>
+      </div>
+      <button>SUSCRIBIRME</button>
+    </div>
+  );
+};
+
+export default Trainers;
